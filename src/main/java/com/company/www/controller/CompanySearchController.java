bc5dbf08f37ef3e1df0d6b86725dbabce43952b7
Anil Chugh <anil_k_chugh@yahoo.com>
2023-09-20 10:59:30 +0100

    Moved exception handling into controller advice

--- src/main/java/com/company/www/controller/CompanySearchController.java
+++ src/main/java/com/company/www/controller/CompanySearchController.java
@@ -9,13 +9,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/www.company.com")
@@ -38,19 +34,6 @@ public class CompanySearchController {
             return new ResponseEntity(companySearchResult, HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 
 }
